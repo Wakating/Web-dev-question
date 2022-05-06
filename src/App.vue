54f60fea763e9db9455e4fd942d83ffdaa9dbf63
Rukudzo Wakandigara <52113767+Wakating@users.noreply.github.com>
2022-05-06 18:55:57 +0200

    added files

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <input
+     type="number"     v-model="limit"    @input="updateValue" />
+    <Numbers/>
+  </div>
+</template>
+
+<script>
+import Numbers from '@/components/Numbers';
+
+export default {
+  name: 'App',
+  components: {Numbers},
+  data()
+  {
+    return {
+      limit: 100
+    }
+  },
+  methods: {
+    updateValue(event){
+
+      const value = event.target.value
+      if (value>100) {
+          this.limit = 100
+      }
+       if (value<0) {
+          this.limit = 0
+      }
+
+
+
+    }
+  }
+
+
+}
+</script>
+
+<style scoped>
+</style>
